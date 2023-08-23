e1e61b434233386c03bf004e15e43f75cdc91a24
Stardust·减 <2225664821@qq.com>
2023-08-23 16:43:01 +0800

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -141,7 +141,7 @@ def run(rank, n_gpus, hps):
         eps=hps.train.eps)
    if net_dur_disc is not None:
         optim_dur_disc = torch.optim.AdanW(
-        net_dur_disc.parametrs(),
+        net_dur_disc.parameters(),
         hps.train.learning_rate,
         betas=hps.train.betas,
         eps=hps.train.eps)
