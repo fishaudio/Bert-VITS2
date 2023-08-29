253f1479f5a2263365e7331c7e4f21c3ed579b10
Stardust·减 <2225664821@qq.com>
2023-08-29 14:47:13 +0800

    优化重采样

--- resample.py
+++ resample.py
@@ -11,7 +11,6 @@ from tqdm import tqdm
 
 def process(item):
     spkdir, wav_name, args = item
-    # speaker 's5', 'p280', 'p315' are excluded,
     speaker = spkdir.replace("\\", "/").split("/")[-1]
     wav_path = os.path.join(args.in_dir, speaker, wav_name)
     if os.path.exists(wav_path) and '.wav' in wav_path:
@@ -27,11 +26,12 @@ def process(item):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sr2", type=int, default=22050, help="sampling rate")
+    parser.add_argument("--sr", type=int, default=44100, help="sampling rate")
     parser.add_argument("--in_dir", type=str, default="./raw", help="path to source dir")
-    parser.add_argument("--out_dir2", type=str, default="./dataset", help="path to target dir")
+    parser.add_argument("--out_dir", type=str, default="./dataset", help="path to target dir")
     args = parser.parse_args()
-    processs = 8
+    # processs = 8
+    processs = cpu_count()-2 if cpu_count() >4 else 1
     pool = Pool(processes=processs)
 
     for speaker in os.listdir(args.in_dir):
