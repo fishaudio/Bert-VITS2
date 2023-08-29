df34d6f78a9f2a19af29071af069bd49fbcabed6
Stardust·减 <2225664821@qq.com>
2023-08-29 20:48:05 +0800

    Update models.py

--- models.py
+++ models.py
@@ -669,7 +669,8 @@ class SynthesizerTrn(nn.Module):
         l_length_sdp = self.sdp(x, x_mask, w, g=g)
         l_length_sdp = l_length_sdp / torch.sum(x_mask)
         
-        logw_ = torch.log(w + 1e-6) * x_mask
+        #logw_ = torch.log(w + 1e-6) * x_mask
+        logw_ = torch.log((w-torch.rand_like(w)) * x_mask + 1e-6) * x_mask
         logw = self.dp(x, x_mask, g=g)
         l_length_dp = torch.sum((logw - logw_) ** 2, [1, 2]) / torch.sum(x_mask)  # for averaging
 
