2d2adb48119f853543ac396ef64cd21e67386519
Stardust·减 <star_dust_chen@foxmail.com>
2023-09-05 00:52:26 +0800

    Update __init__.py

--- text/__init__.py
+++ text/__init__.py
@@ -20,9 +20,11 @@ def cleaned_text_to_sequence(cleaned_text, tones, language):
 def get_bert(norm_text, word2ph, language):
   from .chinese_bert import get_bert_feature as zh_bert
   from .english_bert_mock import get_bert_feature as en_bert
+  from .japanese_bert import get_bert_feature as ja_bert
   lang_bert_func_map = {
     'ZH': zh_bert,
-    'EN': en_bert
+    'EN': en_bert,
+    'JA': ja_bert
   }
   bert = lang_bert_func_map[language](norm_text, word2ph)
   return bert
