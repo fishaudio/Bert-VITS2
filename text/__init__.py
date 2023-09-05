eb14f4a0b77a8effb10af1cf59854ad3ecd4cb34
Stardust·减 <star_dust_chen@foxmail.com>
2023-09-05 19:35:21 +0800

    多卡预处理

--- text/__init__.py
+++ text/__init__.py
@@ -17,7 +17,7 @@ def cleaned_text_to_sequence(cleaned_text, tones, language):
   lang_ids = [lang_id for i in phones]
   return phones, tones, lang_ids
 
-def get_bert(norm_text, word2ph, language):
+def get_bert(norm_text, word2ph, language, device):
   from .chinese_bert import get_bert_feature as zh_bert
   from .english_bert_mock import get_bert_feature as en_bert
   from .japanese_bert import get_bert_feature as ja_bert
@@ -26,5 +26,5 @@ def get_bert(norm_text, word2ph, language):
     'EN': en_bert,
     'JA': ja_bert
   }
-  bert = lang_bert_func_map[language](norm_text, word2ph)
+  bert = lang_bert_func_map[language](norm_text, word2ph, device)
   return bert
