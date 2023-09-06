11a6e9aad76913916d6e563771eb18fafea7fb5a
Stardust·减 <star_dust_chen@foxmail.com>
2023-09-06 20:32:57 +0800

    disable TF32

--- train_ms.py
+++ train_ms.py
@@ -37,9 +37,6 @@ from mel_processing import mel_spectrogram_torch, spec_to_mel_torch
 from text.symbols import symbols
 
 torch.backends.cudnn.benchmark = True
-torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True  # If encontered training problem,please try to disable TF32.
-torch.set_float32_matmul_precision('medium')
 torch.backends.cuda.sdp_kernel("flash")
 torch.backends.cuda.enable_flash_sdp(True)
 torch.backends.cuda.enable_mem_efficient_sdp(True)  # Not avaliable if torch version is lower than 2.0
