ada7d800938ada308a159b543bc2fd1b8df01ad4
Akito-UzukiP <12011903@mail.sustech.edu.cn>
2023-10-12 18:44:06 +0800

    修复日语cleaner和bert

--- text/japanese_bert.py
+++ text/japanese_bert.py
@@ -1,6 +1,8 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 import sys
+import os
+from text.japanese import text2sep_kata
 
 tokenizer = AutoTokenizer.from_pretrained("./bert/bert-base-japanese-v3")
 
@@ -8,6 +10,16 @@ models = dict()
 
 
 def get_bert_feature(text, word2ph, device=None):
+    sep_text,_ = text2sep_kata(text)
+    sep_tokens = [tokenizer.tokenize(t) for t in sep_text]
+    sep_ids = [tokenizer.convert_tokens_to_ids(t) for t in sep_tokens]
+    sep_ids = [2]+[item for sublist in sep_ids for item in sublist]+[3]
+    return get_bert_feature_with_token(sep_ids, word2ph, device)
+
+
+
+
+def get_bert_feature_with_token(tokens, word2ph, device=None):
     if (
         sys.platform == "darwin"
         and torch.backends.mps.is_available()
@@ -21,9 +33,14 @@ def get_bert_feature(text, word2ph, device=None):
             "./bert/bert-base-japanese-v3"
         ).to(device)
     with torch.no_grad():
-        inputs = tokenizer(text, return_tensors="pt")
-        for i in inputs:
-            inputs[i] = inputs[i].to(device)
+        inputs = torch.tensor(tokens).to(device).unsqueeze(0)
+        token_type_ids = torch.zeros_like(inputs).to(device)
+        attention_mask = torch.ones_like(inputs).to(device)
+        inputs = {"input_ids": inputs, "token_type_ids": token_type_ids, "attention_mask": attention_mask}
+
+
+        # for i in inputs:
+        #     inputs[i] = inputs[i].to(device)
         res = models[device](**inputs, output_hidden_states=True)
         res = torch.cat(res["hidden_states"][-3:-2], -1)[0].cpu()
     assert inputs["input_ids"].shape[-1] == len(word2ph)
@@ -36,3 +53,5 @@ def get_bert_feature(text, word2ph, device=None):
     phone_level_feature = torch.cat(phone_level_feature, dim=0)
 
     return phone_level_feature.T
+
+
