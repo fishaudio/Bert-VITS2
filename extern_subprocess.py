0d7ea9b07fba67acbd413d585ced7e9972af2a57
spicysama <122108331+AnyaCoder@users.noreply.github.com>
2023-10-15 12:57:06 +0800

    Update extern_subprocess.py

--- extern_subprocess.py
+++ extern_subprocess.py
@@ -4,19 +4,11 @@ import re
 import multiprocessing
 import json
 import platform
-
 lang_dict = {"EN(英文)": "_en", "ZH(中文)": "_zh", "JP(日语)": "_jp"}
 
-
-def update_json(
-    batch_size: int,
-    log_interval: int,
-    eval_interval: int,
-    epochs: int,
-    lr: float,
-    keep_ckpts: int,
-):
-    with open("configs/config.json", "r", encoding="utf-8") as json_file:
+def update_json(batch_size:int, log_interval:int, eval_interval:int,
+                epochs:int, lr:float, keep_ckpts:int):
+    with open("configs/config.json", "r", encoding='utf-8') as json_file:
         hps = json.load(json_file)
     hps["train"]["batch_size"] = batch_size
     hps["train"]["log_interval"] = log_interval
@@ -24,11 +16,8 @@ def update_json(
     hps["train"]["epochs"] = epochs
     hps["train"]["lr"] = lr
     hps["train"]["keep_ckpts"] = keep_ckpts
-    print(
-        "现在的[BS,LI,EI,epochs,lr,keep]: ",
-        [batch_size, log_interval, eval_interval, epochs, lr, keep_ckpts],
-    )
-    with open("configs/config.json", "w", encoding="utf-8") as json_file:
+    print("现在的[BS,LI,EI,epochs,lr,keep]: ", [batch_size, log_interval, eval_interval, epochs, lr, keep_ckpts])
+    with open("configs/config.json", "w", encoding='utf-8') as json_file:
         json.dump(hps, json_file, indent=4)
     print("config.json文件已更新")
 
@@ -39,8 +28,11 @@ class SubprocessManager:
 
     def worker(self, command):
         try:
-            result = subprocess.check_output(command, universal_newlines=True)
-            result = re.sub(r"\x1B\[[0-?]*[ -/]*[@-~]", "", result)
+            result = subprocess.check_output(
+                command,
+                universal_newlines=True
+            )
+            result = re.sub(r'\x1B\[[0-?]*[ -/]*[@-~]', '', result)
             print(result)
         except subprocess.CalledProcessError as e:
             print(f"错误: {str(e)}")
@@ -68,16 +60,8 @@ class SubprocessManager:
 
 managers = [SubprocessManager() for _ in range(7)]
 
-
 def do_transcribe(target_path, language, workers):
-    additional_args = [
-        "-f",
-        target_path + lang_dict[language],
-        "-l",
-        language,
-        "-w",
-        str(workers),
-    ]
+    additional_args = ["-f", target_path, "-l", language, "-w", str(workers)]
     command = [r"python", "asr_transcript.py"]
     command.extend(additional_args)
     os.environ["SELECT_LANGUAGE"] = language
@@ -93,13 +77,11 @@ def do_preprocess_text(target_path=""):
     print("生成训练集和验证集成功！")
     return "生成训练集和验证集成功！"
 
-
 def do_resample(target_path=""):
     command = [r"python", "resample.py"]
     managers[2].start(command)
     print("重采样完成！")
     return "重采样完成！"
-
 
 def do_bert_gen(num_processes, target_path=""):
     command = [r"python", "bert_gen.py"]
@@ -108,38 +90,28 @@ def do_bert_gen(num_processes, target_path=""):
     print("bert生成完成！")
     return "bert生成完成！"
 
-
 def terminate_training():
     managers[4].terminate()
     print("终止训练！")
     return "终止训练！"
 
-
-def do_training(
-    model_folder: str,
-    batch_size: int,
-    log_interval: int,
-    eval_interval: int,
-    epochs: int,
-    lr: float,
-    keep_ckpts: int,
-):
+def do_training(model_folder:str, batch_size:int, log_interval:int, eval_interval:int,
+                epochs:int, lr:float, keep_ckpts:int):
     update_json(batch_size, log_interval, eval_interval, epochs, lr, keep_ckpts)
     command = [r"python", "train_ms.py"]
-    command.extend(["-m", model_folder, "-c", "./configs/config.json"])
+    command.extend(["-m", model_folder,
+                    "-c", './configs/config.json'])
     terminate_training()
     managers[4].start(command)
     print("开启训练成功！\n http://127.0.0.1:8000")
     return "开启训练成功！\n http://127.0.0.1:8000"
-
 
 def terminate_webui():
     managers[5].terminate()
     print("关闭推理页面！")
     return "关闭推理页面！"
 
-
-def do_inference_webui(model_path: str, config_path: str):
+def do_inference_webui(model_path:str, config_path:str):
     command = [r"python", "webui.py"]
     command.extend(["-m", model_path, "-c", config_path])
     if not os.path.exists(model_path):
@@ -152,7 +124,7 @@ def do_inference_webui(model_path: str, config_path: str):
     return "开启推理页面成功 \n http://127.0.0.1:7860"
 
 
-def do_test(model_path: str = ""):
+def do_test(model_path:str=""):
     command = [r"python", "-m", "pip", "list"]
     managers[6].start(command)
     return "正在测试，请看控制台"
