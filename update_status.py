068106754816f156c0cfdaa1da8aac3111f56a1b
spicysama <122108331+AnyaCoder@users.noreply.github.com>
2023-10-17 13:26:20 +0800

    Add files via upload

--- update_status.py
+++ update_status.py
@@ -1,6 +1,14 @@
 import os
 import gradio as gr
 
+lang_dict = {"EN(英文)": "_en", "ZH(中文)": "_zh", "JP(日语)": "_jp"}
+def raw_dir_convert_to_path(target_dir:str, lang):
+    res = target_dir.rstrip('/').rstrip('\\')
+    if (not target_dir.startswith("raw")) and (not target_dir.startswith("./raw")):
+        res = os.path.join("./raw", res)
+    if (not res.endswith("_zh")) and (not res.endswith("_jp")) and (not res.endswith("_en")):
+        res += lang_dict[lang]
+    return res
 
 def update_g_files():
     g_files = []
@@ -25,7 +33,6 @@ def update_c_files():
     print(c_files)
     return f"更新模型列表完成, 共找到{cnt}个配置文件", gr.Dropdown.update(choices=c_files)
 
-
 def update_model_folders():
     subdirs = []
     cnt = 0
@@ -44,13 +51,12 @@ def update_wav_lab_pairs():
         for file in files:
             # print(file)
             file_path = os.path.join(root, file)
-            if file.lower().endswith(".wav"):
-                lab_file = os.path.splitext(file_path)[0] + ".lab"
+            if file.lower().endswith('.wav'):
+                lab_file = os.path.splitext(file_path)[0] + '.lab'
                 if os.path.exists(lab_file):
                     wav_count += 1
                 tot_count += 1
     return f"{wav_count} / {tot_count}"
-
 
 def update_raw_folders():
     subdirs = []
@@ -61,14 +67,11 @@ def update_raw_folders():
     os.makedirs(raw_path, exist_ok=True)
     for root, dirs, files in os.walk(raw_path):
         for dir_name in dirs:
-            relative_path = os.path.relpath(
-                os.path.join(root, dir_name), script_path
-            )  # 获取相对路径
+            relative_path = os.path.relpath(os.path.join(root, dir_name), script_path)  # 获取相对路径
             subdirs.append(relative_path)
             cnt += 1
     print(subdirs)
-    return (
-        f"更新raw音频文件夹列表完成, 共找到{cnt}个文件夹",
-        gr.Dropdown.update(choices=subdirs),
-        gr.Textbox.update(value=update_wav_lab_pairs()),
-    )
+    return f"更新raw音频文件夹列表完成, 共找到{cnt}个文件夹", \
+        gr.Dropdown.update(choices=subdirs), \
+        gr.Textbox.update(value=update_wav_lab_pairs())
+
