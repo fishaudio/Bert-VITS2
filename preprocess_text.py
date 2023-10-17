397f8bd7a104a57a4977144d053407657d5ea3ab
spicysama <122108331+AnyaCoder@users.noreply.github.com>
2023-10-17 18:00:27 +0800

    Update preprocess_text.py

--- preprocess_text.py
+++ preprocess_text.py
@@ -1,5 +1,4 @@
 import json
-import os.path
 from collections import defaultdict
 from random import shuffle
 from typing import Optional
@@ -26,7 +25,7 @@ from text.cleaner import clean_text
 @click.option("--val-per-spk", default=4)
 @click.option("--max-val-total", default=8)
 @click.option("--clean/--no-clean", default=True)
-def main(
+def preprocess(
     transcription_path: str,
     cleaned_path: Optional[str],
     train_path: str,
@@ -40,55 +39,44 @@ def main(
         cleaned_path = transcription_path + ".cleaned"
 
     if clean:
-        out_file = open(cleaned_path, "w", encoding="utf-8")
-        for line in tqdm(open(transcription_path, encoding="utf-8").readlines()):
-            try:
-                utt, spk, language, text = line.strip().split("|")
-                norm_text, phones, tones, word2ph = clean_text(text, language)
-                out_file.write(
-                    "{}|{}|{}|{}|{}|{}|{}\n".format(
-                        utt,
-                        spk,
-                        language,
-                        norm_text,
-                        " ".join(phones),
-                        " ".join([str(i) for i in tones]),
-                        " ".join([str(i) for i in word2ph]),
-                    )
-                )
-            except Exception as error:
-                print("err!", line, error)
+        with open(cleaned_path, "w", encoding="utf-8") as out_file:
+            with open(transcription_path, "r", encoding="utf-8") as trans_file:
+                lines = trans_file.readlines()
+                print(lines, ' ', len(lines))
+                if len(lines) != 0:
+                    for line in tqdm(lines):
+                        try:
+                            utt, spk, language, text = line.strip().split("|")
+                            norm_text, phones, tones, word2ph = clean_text(text, language)
+                            out_file.write(
+                                "{}|{}|{}|{}|{}|{}|{}\n".format(
+                                    utt,
+                                    spk,
+                                    language,
+                                    norm_text,
+                                    " ".join(phones),
+                                    " ".join([str(i) for i in tones]),
+                                    " ".join([str(i) for i in word2ph]),
+                                )
+                            )
+                        except Exception as e:
+                            print("生成训练集和验证集时发生错误！")
 
-        out_file.close()
 
-        transcription_path = cleaned_path
 
+    transcription_path = cleaned_path
     spk_utt_map = defaultdict(list)
     spk_id_map = {}
     current_sid = 0
 
-    with open(transcription_path, encoding="utf-8") as f:
-        audioPaths = set()
-        countSame = 0
-        countNotFound = 0
+    with open(transcription_path, "r", encoding="utf-8") as f:
         for line in f.readlines():
             utt, spk, language, text, phones, tones, word2ph = line.strip().split("|")
-            if utt in audioPaths:
-                # 过滤数据集错误：相同的音频匹配多个文本，导致后续bert出问题
-                print(f"重复音频文本：{line}")
-                countSame += 1
-                continue
-            if not os.path.isfile(utt):
-                print(f"没有找到对应的音频：{utt}")
-                countNotFound += 1
-                continue
-            audioPaths.add(utt)
             spk_utt_map[spk].append(line)
 
             if spk not in spk_id_map.keys():
                 spk_id_map[spk] = current_sid
                 current_sid += 1
-        print(f"总重复音频数：{countSame}，总未找到的音频数:{countNotFound}")
 
     train_list = []
     val_list = []
@@ -114,7 +102,9 @@ def main(
     config["data"]["spk2id"] = spk_id_map
     with open(config_path, "w", encoding="utf-8") as f:
         json.dump(config, f, indent=2, ensure_ascii=False)
+    print("训练集和验证集生成完成！")
 
 
 if __name__ == "__main__":
-    main()
+    preprocess()
+
