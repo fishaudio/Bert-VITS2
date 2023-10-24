fcc3cc4e93211feb1575b08d527324118403846c
Sihan Wang <wangsihan1995@gmail.com>
2023-10-24 23:37:55 +0800

    Add reference audio

--- emo_gen.py
+++ emo_gen.py
@@ -63,6 +63,7 @@ def process_func(
     sampling_rate: int,
     model: EmotionModel,
     processor: Wav2Vec2Processor,
+    device: str,
     embeddings: bool = False,
 ) -> np.ndarray:
     r"""Predict emotions or extract embeddings from raw audio signal."""
@@ -85,7 +86,23 @@ def process_func(
     return y
 
 
-def extract_dir(data_queue, model, processor):
+def get_emo(meta):
+    sr, data = meta
+    device = config.bert_gen_config.device
+    model_name = "./emotional/wav2vec2-large-robust-12-ft-emotion-msp-dim"
+    processor = Wav2Vec2Processor.from_pretrained(model_name)
+    model = EmotionModel.from_pretrained(model_name)
+    return process_func(
+        np.expand_dims(data, 0).astype(np.float),
+        sr,
+        model,
+        processor,
+        device,
+        embeddings=True,
+    ).squeeze(0)
+
+
+def extract_dir(data_queue, model, processor, device):
     while not data_queue.empty():
         data = data_queue.get()
         wavname = data.split("|")[0]  # 获取每一行的第一部分
@@ -94,7 +111,7 @@ def extract_dir(data_queue, model, processor):
             continue
         wav, sr = librosa.load(wavname, 16000)
         emb = process_func(
-            np.expand_dims(wav, 0), sr, model, processor, embeddings=True
+            np.expand_dims(wav, 0), sr, model, processor, device, embeddings=True
         )
         np.save(emo_path, emb.squeeze(0))
         print(f"{emo_path} 生成完毕！")
@@ -135,7 +152,7 @@ if __name__ == "__main__":
         queues[i % args.num_processes].put(line)
 
     for i in range(args.num_processes):  # 创建工作进程
-        p = mp.Process(target=extract_dir, args=(queues[i], model, processor))
+        p = mp.Process(target=extract_dir, args=(queues[i], model, processor, device))
         p.start()
         processes.append(p)
 
@@ -143,4 +160,4 @@ if __name__ == "__main__":
     for p in processes:
         p.join()
 
-    print("Emo vec 生成完毕!")
+    print(f"Emo vec 生成完毕!")
--- infer.py
+++ infer.py
@@ -15,6 +15,7 @@ import utils
 
 from models import SynthesizerTrn
 from text.symbols import symbols
+from emo_gen import get_emo
 from oldVersion.V111.models import SynthesizerTrn as V111SynthesizerTrn
 from oldVersion.V111.text import symbols as V111symbols
 from oldVersion.V110.models import SynthesizerTrn as V110SynthesizerTrn
@@ -72,7 +73,7 @@ def get_net_g(model_path: str, version: str, device: str, hps):
     return net_g
 
 
-def get_text(text, language_str, hps, device):
+def get_text(text, reference_audio, language_str, hps, device):
     # 在此处实现当前版本的get_text
     norm_text, phone, tone, word2ph = clean_text(text, language_str)
     phone, tone, language = cleaned_text_to_sequence(phone, tone, language_str)
@@ -90,13 +91,18 @@ def get_text(text, language_str, hps, device):
 
     if language_str == "ZH":
         bert = bert
-        ja_bert = torch.zeros(768, len(phone))
+        ja_bert = torch.zeros(1024, len(phone))
+        en_bert = torch.zeros(768, len(phone))
     elif language_str == "JP":
+        bert = torch.zeros(1024, len(phone))
         ja_bert = bert
+        en_bert = torch.zeros(768, len(phone))
+    elif language_str == "EN":
         bert = torch.zeros(1024, len(phone))
-    else:
-        bert = torch.zeros(1024, len(phone))
-        ja_bert = torch.zeros(768, len(phone))
+        ja_bert = torch.zeros(1024, len(phone))
+        en_bert = bert
+
+    emo = torch.from_numpy(get_emo(reference_audio))
 
     assert bert.shape[-1] == len(
         phone
@@ -105,11 +111,12 @@ def get_text(text, language_str, hps, device):
     phone = torch.LongTensor(phone)
     tone = torch.LongTensor(tone)
     language = torch.LongTensor(language)
-    return bert, ja_bert, phone, tone, language
+    return bert, ja_bert, en_bert, emo, phone, tone, language
 
 
 def infer(
     text,
+    reference_audio,
     sdp_ratio,
     noise_scale,
     noise_scale_w,
@@ -163,13 +170,17 @@ def infer(
                 device,
             )
     # 在此处实现当前版本的推理
-    bert, ja_bert, phones, tones, lang_ids = get_text(text, language, hps, device)
+    bert, ja_bert, en_bert, emo, phones, tones, lang_ids = get_text(
+        text, reference_audio, language, hps, device
+    )
     with torch.no_grad():
         x_tst = phones.to(device).unsqueeze(0)
         tones = tones.to(device).unsqueeze(0)
         lang_ids = lang_ids.to(device).unsqueeze(0)
         bert = bert.to(device).unsqueeze(0)
         ja_bert = ja_bert.to(device).unsqueeze(0)
+        en_bert = en_bert.to(device).unsqueeze(0)
+        emo = emo.to(device).unsqueeze(0)
         x_tst_lengths = torch.LongTensor([phones.size(0)]).to(device)
         del phones
         speakers = torch.LongTensor([hps.data.spk2id[sid]]).to(device)
@@ -182,6 +193,8 @@ def infer(
                 lang_ids,
                 bert,
                 ja_bert,
+                en_bert,
+                emo,
                 sdp_ratio=sdp_ratio,
                 noise_scale=noise_scale,
                 noise_scale_w=noise_scale_w,
