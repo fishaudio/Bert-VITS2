781e3188b67e243d97f55deba1316cdfa356d5d1
OedoSoldier <31711261+OedoSoldier@users.noreply.github.com>
2023-10-28 12:19:25 +0800

    Update emo_gen.py

--- emo_gen.py
+++ emo_gen.py
@@ -11,7 +11,21 @@ import argparse
 from config import config
 import utils
 import os
-import torch.multiprocessing as mp
+from pqdm.processes import pqdm
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-c", "--config", type=str, default=config.bert_gen_config.config_path
+)
+parser.add_argument(
+    "--num_processes", type=int, default=config.bert_gen_config.num_processes
+)
+args, _ = parser.parse_known_args()
+config_path = args.config
+hps = utils.get_hparams_from_file(config_path)
+
+device = config.bert_gen_config.device
 
 
 class RegressionHead(nn.Module):
@@ -58,27 +72,31 @@ class EmotionModel(Wav2Vec2PreTrainedModel):
         return hidden_states, logits
 
 
+model_name = "./emotional/wav2vec2-large-robust-12-ft-emotion-msp-dim"
+global processor, model
+processor = Wav2Vec2Processor.from_pretrained(model_name)
+model = EmotionModel.from_pretrained(model_name)
+
+
 def process_func(
     x: np.ndarray,
     sampling_rate: int,
-    model: EmotionModel,
-    processor: Wav2Vec2Processor,
-    device: str,
     embeddings: bool = False,
 ) -> np.ndarray:
     r"""Predict emotions or extract embeddings from raw audio signal."""
     # run through processor to normalize signal
     # always returns a batch, so we just get the first entry
     # then we put it on the device
-    model = model.to(device)
+    global model
+    m = model.to(device)
     y = processor(x, sampling_rate=sampling_rate)
     y = y["input_values"][0]
     y = torch.from_numpy(y).unsqueeze(0).to(device)
 
     # run through model
     with torch.no_grad():
-        y = model(y)[0 if embeddings else 1]
-    del model
+        y = m(y)[0 if embeddings else 1]
+    del m
 
     # convert to numpy
     y = y.detach().cpu().numpy()
@@ -86,80 +104,43 @@ def process_func(
     return y
 
 
-model_name = "./emotional/wav2vec2-large-robust-12-ft-emotion-msp-dim"
-processor = Wav2Vec2Processor.from_pretrained(model_name)
-model = EmotionModel.from_pretrained(model_name)
-
-
 def get_emo(path):
     wav, sr = librosa.load(path, 16000)
     device = config.bert_gen_config.device
     return process_func(
-        np.expand_dims(wav, 0).astype(np.float64),
+        np.expand_dims(wav, 0).astype(np.float),
         sr,
-        model,
-        processor,
-        device,
         embeddings=True,
     ).squeeze(0)
 
 
-def extract_dir(data_queue, model, processor, device):
-    while not data_queue.empty():
-        data = data_queue.get()
+def extract_dir(data):
+    try:
         wavname = data.split("|")[0]  # 获取每一行的第一部分
         emo_path = wavname.replace(".wav", ".emo.npy")
         if os.path.exists(emo_path):
-            continue
+            return f"{emo_path} 已存在！"
         wav, sr = librosa.load(wavname, 16000)
-        emb = process_func(
-            np.expand_dims(wav, 0), sr, model, processor, device, embeddings=True
-        )
+        emb = process_func(np.expand_dims(wav, 0), sr, embeddings=True)
         np.save(emo_path, emb.squeeze(0))
-        print(f"{emo_path} 生成完毕！")
         del data, wav, sr, emb
-    return
+    except Exception as e:
+        return e
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-c", "--config", type=str, default=config.emo_gen_config.config_path
-    )
-    parser.add_argument(
-        "--num_processes", type=int, default=config.emo_gen_config.num_processes
-    )
-    args, _ = parser.parse_known_args()
-    config_path = args.config
-    hps = utils.get_hparams_from_file(config_path)
+lines = []
+with open(hps.data.training_files, encoding="utf-8") as f:
+    lines.extend(f.readlines())
 
-    device = config.emo_gen_config.device
-    model_name = "./emotional/wav2vec2-large-robust-12-ft-emotion-msp-dim"
-    processor = Wav2Vec2Processor.from_pretrained(model_name)
-    model = EmotionModel.from_pretrained(model_name)
+with open(hps.data.validation_files, encoding="utf-8") as f:
+    lines.extend(f.readlines())
 
-    lines = []
-    with open(hps.data.training_files, encoding="utf-8") as f:
-        lines.extend(f.readlines())
+errors = pqdm(
+    lines,
+    extract_dir,
+    n_jobs=4,
+)
 
-    with open(hps.data.validation_files, encoding="utf-8") as f:
-        lines.extend(f.readlines())
+print(errors)
 
-    processes = []
-    data_queue = mp.Queue()
-
-    # 将数据按 batch 放入队列
-    queues = [mp.Queue()] * args.num_processes
-    for i, line in enumerate(lines):
-        queues[i % args.num_processes].put(line)
-
-    for i in range(args.num_processes):  # 创建工作进程
-        p = mp.Process(target=extract_dir, args=(queues[i], model, processor, device))
-        p.start()
-        processes.append(p)
-
-    # 等待所有工作进程完成
-    for p in processes:
-        p.join()
-
-    print("Emo vec 生成完毕!")
+print(f"Emo vec 生成完毕!")
