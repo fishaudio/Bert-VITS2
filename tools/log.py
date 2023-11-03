25ea864b56ff022f33f3af96e9ac247e467266dd
Sora <atri@suzakuintsubaki.com>
2023-11-03 13:09:58 +0800

    Update server_fastapi.py. Avoid loading the model repeatedly. Add logger (#145)
    * Update server_fastapi.py. Avoid loading the model repeatedly. Add loguru logger.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tools/log.py
@@ -0,0 +1,14 @@
+"""
+logger封装
+"""
+from loguru import logger
+import sys
+
+
+# 移除所有默认的处理器
+logger.remove()
+
+# 自定义格式并添加到标准输出
+log_format = "<g>{time:MM-DD HH:mm:ss}</g> [<lvl>{level}</lvl>] | {message}"
+
+logger.add(sys.stdout, format=log_format)
