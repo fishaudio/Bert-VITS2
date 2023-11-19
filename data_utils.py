86f224385b02aa2c063652160ce4ff1d9841f591
Stardust·减 <star_dust_chen@foxmail.com>
2023-11-19 17:06:07 +0800

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -41,7 +41,7 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
 
         self.add_blank = hparams.add_blank
         self.min_text_len = getattr(hparams, "min_text_len", 1)
-        self.max_text_len = getattr(hparams, "max_text_len", 512)
+        self.max_text_len = getattr(hparams, "max_text_len", 384)
 
         random.seed(1234)
         random.shuffle(self.audiopaths_sid_text)
