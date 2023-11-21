fe116aabd9c152b321f02a9bf7f529d8ab575580
Stardust·减 <star_dust_chen@foxmail.com>
2023-11-21 11:31:45 +0800

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -54,7 +54,7 @@ def run():
     dist.init_process_group(
         backend=backend,
         init_method="env://",  # If Windows,switch to gloo backend.
-        timeout=datetime.timedelta(seconds=300),
+        timeout=datetime.timedelta(seconds=120),
     )  # Use torchrun instead of mp.spawn
     rank = dist.get_rank()
     local_rank = int(os.environ["LOCAL_RANK"])
