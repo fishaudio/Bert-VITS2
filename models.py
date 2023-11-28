e071a6a2ccb4d059a80187c6e99f8542d625ceb0
Stardust·减 <2225664821@qq.com>
2023-11-28 19:02:47 +0800

    更新 models.py

--- models.py
+++ models.py
@@ -343,7 +343,7 @@ class TextEncoder(nn.Module):
         self.bert_proj = nn.Conv1d(1024, hidden_channels, 1)
         self.ja_bert_proj = nn.Conv1d(1024, hidden_channels, 1)
         self.en_bert_proj = nn.Conv1d(1024, hidden_channels, 1)
-        self.emo_proj = nn.Linear(1024, 1024)
+        self.emo_proj = nn.Linear(1024, hidden_channels)
 
         self.encoder = attentions.Encoder(
             hidden_channels,
