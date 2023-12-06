4365ae2835edc2f225e8408e69d671896191047a
Stardust·减 <star_dust_chen@foxmail.com>
2023-12-06 16:52:00 +0800

    Update webui.py

--- webui.py
+++ webui.py
@@ -425,7 +425,7 @@ if __name__ == "__main__":
                     choices=speakers, value=speakers[0], label="Speaker"
                 )
                 emotion = gr.Slider(
-                    minimum=-1, maximum=9, value=0, step=1, label="Emotion"
+                    minimum=-1, maximum=config.emo_cluster_config.n_clusters-1, value=0, step=1, label="Emotion"
                 )
                 sdp_ratio = gr.Slider(
                     minimum=0, maximum=1, value=0.2, step=0.1, label="SDP Ratio"
