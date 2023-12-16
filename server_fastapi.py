47fcb015ad8fde9e934428b8235b4862648fb413
Stardust·减 <star_dust_chen@foxmail.com>
2023-12-16 20:51:31 +0800

    Feat: Style text: make emotions and style similar to the style text by mixing bert (#240) (#241)
    * fix:(oldVersion210) Load on demand Emotion model
    
    * feat: update fastapi.py. 添加更多错误日志信息
    
    * Switch pyopenjtalk to pyopenjtalk-prebuilt
    
    * fix: update fastapi.py. 2.2 reference适配
    
    * Update resample.py
    
    * 修复Onnx导出的BUG (#237)
    
    * Add files via upload
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add files via upload
    
    * Add files via upload
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Delete attentions_onnx.py
    
    * Delete models_onnx.py
    
    * Add files via upload
    
    * Add files via upload
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    
    
    * Fix onnx
    
    * Format export
    
    * Feat: style-text and bert mixing (JA only)
    
    * Ensure the same tensor shape
    
    * Update
    
    * update gradio version
    
    * Fix
    
    * Style text for chinese and english (ver 2.2)
    
    * Style text for chinese and english (ver 2.1)
    
    * Style text in FastAPI
    
    * Translate style text desc in chinese
    
    ---------
    
    Co-authored-by: litagin02 <139731664+litagin02@users.noreply.github.com>
    Co-authored-by: Sora <654163754@qq.com>
    Co-authored-by: Sihan Wang <wangsihan1995@gmail.com>
    Co-authored-by: Ναρουσέ·μ·γιουμεμί·Χινακάννα <40709280+NaruseMioShirakana@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- server_fastapi.py
+++ server_fastapi.py
@@ -5,6 +5,7 @@ import logging
 import gc
 import random
 
+import librosa
 import gradio
 import numpy as np
 import utils
@@ -203,28 +204,48 @@ if __name__ == "__main__":
         auto_split: bool,
         emotion: Optional[Union[int, str]] = None,
         reference_audio=None,
+        style_text: Optional[str] = None,
+        style_weight: float = 0.7,
     ) -> Union[Response, Dict[str, any]]:
         """TTS实现函数"""
         # 检查模型是否存在
         if model_id not in loaded_models.models.keys():
+            logger.error(f"/voice 请求错误：模型model_id={model_id}未加载")
             return {"status": 10, "detail": f"模型model_id={model_id}未加载"}
         # 检查是否提供speaker
         if speaker_name is None and speaker_id is None:
+            logger.error("/voice 请求错误：推理请求未提供speaker_name或speaker_id")
             return {"status": 11, "detail": "请提供speaker_name或speaker_id"}
         elif speaker_name is None:
             # 检查speaker_id是否存在
             if speaker_id not in loaded_models.models[model_id].id2spk.keys():
+                logger.error(f"/voice 请求错误：角色speaker_id={speaker_id}不存在")
                 return {"status": 12, "detail": f"角色speaker_id={speaker_id}不存在"}
             speaker_name = loaded_models.models[model_id].id2spk[speaker_id]
         # 检查speaker_name是否存在
         if speaker_name not in loaded_models.models[model_id].spk2id.keys():
+            logger.error(f"/voice 请求错误：角色speaker_name={speaker_name}不存在")
             return {"status": 13, "detail": f"角色speaker_name={speaker_name}不存在"}
+        # 未传入则使用默认语言
         if language is None:
             language = loaded_models.models[model_id].language
+        # 翻译会破坏mix结构，auto也会变得无意义。不要在这两个模式下使用
         if auto_translate:
+            if language == "auto" or language == "mix":
+                logger.error(
+                    f"/voice 请求错误：请勿同时使用language = {language}与auto_translate模式"
+                )
+                return {
+                    "status": 20,
+                    "detail": f"请勿同时使用language = {language}与auto_translate模式",
+                }
             text = trans.translate(Sentence=text, to_Language=language.lower())
         if reference_audio is not None:
             ref_audio = BytesIO(await reference_audio.read())
+            # 2.2 适配
+            if loaded_models.models[model_id].version == "2.2":
+                ref_audio, _ = librosa.load(ref_audio, 48000)
+
         else:
             ref_audio = reference_audio
         if not auto_split:
@@ -242,6 +263,8 @@ if __name__ == "__main__":
                     device=loaded_models.models[model_id].device,
                     emotion=emotion,
                     reference_audio=ref_audio,
+                    style_text=style_text,
+                    style_weight=style_weight,
                 )
                 audio = gradio.processing_utils.convert_to_16_bit_wav(audio)
         else:
@@ -263,6 +286,8 @@ if __name__ == "__main__":
                             device=loaded_models.models[model_id].device,
                             emotion=emotion,
                             reference_audio=ref_audio,
+                            style_text=style_text,
+                            style_weight=style_weight,
                         )
                     )
                     audios.append(np.zeros(int(44100 * 0.2)))
@@ -293,6 +318,8 @@ if __name__ == "__main__":
         auto_split: bool = Query(False, description="自动切分"),
         emotion: Optional[Union[int, str]] = Query(None, description="emo"),
         reference_audio: UploadFile = File(None),
+        style_text: Optional[str] = Form(None, description="风格文本"),
+        style_weight: float = Query(0.7, description="风格权重"),
     ):
         """语音接口，若需要上传参考音频请仅使用post请求"""
         logger.info(
@@ -312,6 +339,8 @@ if __name__ == "__main__":
             auto_split=auto_split,
             emotion=emotion,
             reference_audio=reference_audio,
+            style_text=style_text,
+            style_weight=style_weight,
         )
 
     @app.get("/voice")
@@ -331,6 +360,8 @@ if __name__ == "__main__":
         auto_translate: bool = Query(False, description="自动翻译"),
         auto_split: bool = Query(False, description="自动切分"),
         emotion: Optional[Union[int, str]] = Query(None, description="emo"),
+        style_text: Optional[str] = Query(None, description="风格文本"),
+        style_weight: float = Query(0.7, description="风格权重"),
     ):
         """语音接口"""
         logger.info(
@@ -349,6 +380,8 @@ if __name__ == "__main__":
             auto_translate=auto_translate,
             auto_split=auto_split,
             emotion=emotion,
+            style_text=style_text,
+            style_weight=style_weight,
         )
 
     @app.get("/models/info")
@@ -370,7 +403,9 @@ if __name__ == "__main__":
         )
         result = loaded_models.del_model(model_id)
         if result is None:
+            logger.error(f"/models/delete 模型删除错误：模型{model_id}不存在，删除失败")
             return {"status": 14, "detail": f"模型{model_id}不存在，删除失败"}
+
         return {"status": 0, "detail": "删除成功"}
 
     @app.get("/models/add")
@@ -394,6 +429,7 @@ if __name__ == "__main__":
             elif os.path.isfile(os.path.join(model_dir, "../config.json")):
                 config_path = os.path.join(model_dir, "../config.json")
             else:
+                logger.error("/models/add 模型添加失败：未在模型所在目录以及上级目录找到config.json文件")
                 return {
                     "status": 15,
                     "detail": "查询未传入配置文件路径，同时默认路径./与../中不存在配置文件config.json。",
@@ -628,8 +664,10 @@ if __name__ == "__main__":
             f"{request.client.host}:{request.client.port}/tools/get_audio  { unquote(str(request.query_params) )}"
         )
         if not os.path.isfile(path):
+            logger.error(f"/tools/get_audio 获取音频错误：指定音频{path}不存在")
             return {"status": 18, "detail": "指定音频不存在"}
-        if not path.endswith(".wav"):
+        if not path.lower().endswith(".wav"):
+            logger.error(f"/tools/get_audio 获取音频错误：音频{path}非wav文件")
             return {"status": 19, "detail": "非wav格式文件"}
         return FileResponse(path=path)
 
--- text/__init__.py
+++ text/__init__.py
@@ -18,13 +18,15 @@ def cleaned_text_to_sequence(cleaned_text, tones, language):
     return phones, tones, lang_ids
 
 
-def get_bert(norm_text, word2ph, language, device):
+def get_bert(norm_text, word2ph, language, device, style_text=None, style_weight=0.7):
     from .chinese_bert import get_bert_feature as zh_bert
     from .english_bert_mock import get_bert_feature as en_bert
     from .japanese_bert import get_bert_feature as jp_bert
 
     lang_bert_func_map = {"ZH": zh_bert, "EN": en_bert, "JP": jp_bert}
-    bert = lang_bert_func_map[language](norm_text, word2ph, device)
+    bert = lang_bert_func_map[language](
+        norm_text, word2ph, device, style_text, style_weight
+    )
     return bert
 
 
