bd4591aa2c3d19daffd4444c11c734a98e30b2f6
Sihan Wang <wangsihan1995@gmail.com>
2023-12-20 02:56:32 +0800

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -387,6 +387,7 @@ def run():
             )
         scheduler_g.step()
         scheduler_d.step()
+        scheduler_wd.step()
         if net_dur_disc is not None:
             scheduler_dur_disc.step()
 
