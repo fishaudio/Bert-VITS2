e3b308ebeefa741fa293e929b412957ef1d4b6ea
Stardust·减 <star_dust_chen@foxmail.com>
2023-12-23 11:32:17 +0800

    Update __init__.py

--- text/__init__.py
+++ text/__init__.py
@@ -20,17 +20,10 @@ def cleaned_text_to_sequence(cleaned_text, tones, language):
 
 def get_bert(norm_text, word2ph, language, device, style_text=None, style_weight=0.7):
     from .chinese_bert import get_bert_feature as zh_bert
-    from .english_bert_mock import get_bert_feature as en_bert
-    from .japanese_bert import get_bert_feature as jp_bert
-
-    lang_bert_func_map = {"ZH": zh_bert, "EN": en_bert, "JP": jp_bert}
-    bert = lang_bert_func_map[language](
+    bert = zh_bert(
         norm_text, word2ph, device, style_text, style_weight
     )
     return bert
-
-
-
 
 def init_openjtalk():
     import platform
