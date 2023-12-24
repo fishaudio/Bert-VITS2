66abf75a37c422c7a2c98d4b24370f79d7d30058
Stardust·减 <star_dust_chen@foxmail.com>
2023-12-24 22:55:42 +0800

    Update models.py

--- models.py
+++ models.py
@@ -361,8 +361,8 @@ class TextEncoder(nn.Module):
         nn.init.normal_(self.tone_emb.weight, 0.0, hidden_channels**-0.5)
         self.language_emb = nn.Embedding(num_languages, hidden_channels)
         nn.init.normal_(self.language_emb.weight, 0.0, hidden_channels**-0.5)
-        self.bert_proj = nn.Conv1d(2560, 1024, 1)
-        self.bert_pre_proj = nn.Conv1d(1024, hidden_channels, 1)
+        self.bert_proj = nn.Conv1d(1024, hidden_channels, 1)
+        self.bert_pre_proj = nn.Conv1d(2560, 1024, 1)
 
         self.encoder = attentions.Encoder(
             hidden_channels,
