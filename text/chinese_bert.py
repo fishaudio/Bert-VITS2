a0bdd455ef292a48b4358e6fd046d9700dcee653
Stardust·减 <star_dust_chen@foxmail.com>
2023-12-25 12:29:59 +0800

    Update chinese_bert.py

--- text/chinese_bert.py
+++ text/chinese_bert.py
@@ -1,11 +1,11 @@
 import sys
 import torch
-from transformers import AutoModelForMaskedLM, AutoTokenizer
+from transformers import MegatronBertModel, BertTokenizer
+
 from config import config
 
-LOCAL_PATH = "./bert/Erlangshen-MegatronBert-3.9B-Chinese"
-tokenizer = AutoTokenizer.from_pretrained(LOCAL_PATH)
-
+LOCAL_PATH = "./bert/Erlangshen-MegatronBert-1.3B"
+tokenizer = BertTokenizer.from_pretrained(LOCAL_PATH)
 models = dict()
 
 
@@ -25,7 +25,7 @@ def get_bert_feature(
     if not device:
         device = "cuda"
     if device not in models.keys():
-        models[device] = AutoModelForMaskedLM.from_pretrained(LOCAL_PATH).to(device)
+        models[device] = MegatronBertModel.from_pretrained(LOCAL_PATH).to(device)
     with torch.no_grad():
         inputs = tokenizer(text, return_tensors="pt")
         for i in inputs:
