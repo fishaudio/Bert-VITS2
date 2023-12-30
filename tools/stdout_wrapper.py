24ac11c564760191dba349d125bc9abe54a0d782
litagin02 <litagin02@gmail.com>
2023-12-30 10:18:54 +0900

    Fix stdout wrapper?

--- tools/stdout_wrapper.py
+++ tools/stdout_wrapper.py
@@ -1,12 +1,29 @@
 import sys
+import tempfile
 
 
 class StdoutWrapper:
+    def __init__(self):
+        # 一時ファイルの作成とオープン
+        self.temp_file = tempfile.NamedTemporaryFile(mode="w+", delete=False)
+
     def write(self, message: str):
-        print(message, end="")
+        # メッセージを一時ファイルに書き込む
+        self.temp_file.write(message)
+        self.temp_file.flush()
 
     def flush(self):
-        pass
+        # 一時ファイルのフラッシュ
+        self.temp_file.flush()
+
+    def read(self):
+        # ファイルの内容を読み出す
+        self.temp_file.seek(0)  # ファイルの先頭にシーク
+        return self.temp_file.read()
+
+    def close(self):
+        # 一時ファイルを閉じる
+        self.temp_file.close()
 
 
 def get_stdout():
