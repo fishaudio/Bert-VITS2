080239918b0bf98f9804451ba4e47f4d7a6e5394
Stardust·减 <star_dust_chen@foxmail.com>
2023-12-31 22:09:35 +0800

    Update infer.py

--- infer.py
+++ infer.py
@@ -112,26 +112,10 @@ def get_text(text, language_str, hps, device, style_text=None, style_weight=0.7)
         for i in range(len(word2ph)):
             word2ph[i] = word2ph[i] * 2
         word2ph[0] += 1
-    bert_ori = get_bert(
+    bert = get_bert(
         norm_text, word2ph, language_str, device, style_text, style_weight
     )
     del word2ph
-    assert bert_ori.shape[-1] == len(phone), phone
-
-    if language_str == "ZH":
-        bert = bert_ori
-        ja_bert = torch.randn(1024, len(phone))
-        en_bert = torch.randn(1024, len(phone))
-    elif language_str == "JP":
-        bert = torch.randn(1024, len(phone))
-        ja_bert = bert_ori
-        en_bert = torch.randn(1024, len(phone))
-    elif language_str == "EN":
-        bert = torch.randn(1024, len(phone))
-        ja_bert = torch.randn(1024, len(phone))
-        en_bert = bert_ori
-    else:
-        raise ValueError("language_str should be ZH, JP or EN")
 
     assert bert.shape[-1] == len(
         phone
@@ -140,7 +124,7 @@ def get_text(text, language_str, hps, device, style_text=None, style_weight=0.7)
     phone = torch.LongTensor(phone)
     tone = torch.LongTensor(tone)
     language = torch.LongTensor(language)
-    return bert, ja_bert, en_bert, phone, tone, language
+    return bert, phone, tone, language
 
 
 def infer(
@@ -251,22 +235,16 @@ def infer(
         tones = tones[3:]
         lang_ids = lang_ids[3:]
         bert = bert[:, 3:]
-        ja_bert = ja_bert[:, 3:]
-        en_bert = en_bert[:, 3:]
     if skip_end:
         phones = phones[:-2]
         tones = tones[:-2]
         lang_ids = lang_ids[:-2]
         bert = bert[:, :-2]
-        ja_bert = ja_bert[:, :-2]
-        en_bert = en_bert[:, :-2]
     with torch.no_grad():
         x_tst = phones.to(device).unsqueeze(0)
         tones = tones.to(device).unsqueeze(0)
         lang_ids = lang_ids.to(device).unsqueeze(0)
         bert = bert.to(device).unsqueeze(0)
-        ja_bert = ja_bert.to(device).unsqueeze(0)
-        en_bert = en_bert.to(device).unsqueeze(0)
         x_tst_lengths = torch.LongTensor([phones.size(0)]).to(device)
         # emo = emo.to(device).unsqueeze(0)
         del phones
@@ -280,8 +258,6 @@ def infer(
                 tones,
                 lang_ids,
                 bert,
-                ja_bert,
-                en_bert,
                 sdp_ratio=sdp_ratio,
                 noise_scale=noise_scale,
                 noise_scale_w=noise_scale_w,
@@ -298,8 +274,6 @@ def infer(
             bert,
             x_tst_lengths,
             speakers,
-            ja_bert,
-            en_bert,
         )  # , emo
         if torch.cuda.is_available():
             torch.cuda.empty_cache()
@@ -334,35 +308,25 @@ def infer_multilang(
         _skip_end = (idx != len(language) - 1) or skip_end
         (
             temp_bert,
-            temp_ja_bert,
-            temp_en_bert,
             temp_phones,
             temp_tones,
             temp_lang_ids,
         ) = get_text(txt, lang, hps, device)
         if _skip_start:
             temp_bert = temp_bert[:, 3:]
-            temp_ja_bert = temp_ja_bert[:, 3:]
-            temp_en_bert = temp_en_bert[:, 3:]
             temp_phones = temp_phones[3:]
             temp_tones = temp_tones[3:]
             temp_lang_ids = temp_lang_ids[3:]
         if _skip_end:
             temp_bert = temp_bert[:, :-2]
-            temp_ja_bert = temp_ja_bert[:, :-2]
-            temp_en_bert = temp_en_bert[:, :-2]
             temp_phones = temp_phones[:-2]
             temp_tones = temp_tones[:-2]
             temp_lang_ids = temp_lang_ids[:-2]
         bert.append(temp_bert)
-        ja_bert.append(temp_ja_bert)
-        en_bert.append(temp_en_bert)
         phones.append(temp_phones)
         tones.append(temp_tones)
         lang_ids.append(temp_lang_ids)
     bert = torch.concatenate(bert, dim=1)
-    ja_bert = torch.concatenate(ja_bert, dim=1)
-    en_bert = torch.concatenate(en_bert, dim=1)
     phones = torch.concatenate(phones, dim=0)
     tones = torch.concatenate(tones, dim=0)
     lang_ids = torch.concatenate(lang_ids, dim=0)
@@ -371,8 +335,6 @@ def infer_multilang(
         tones = tones.to(device).unsqueeze(0)
         lang_ids = lang_ids.to(device).unsqueeze(0)
         bert = bert.to(device).unsqueeze(0)
-        ja_bert = ja_bert.to(device).unsqueeze(0)
-        en_bert = en_bert.to(device).unsqueeze(0)
         # emo = emo.to(device).unsqueeze(0)
         x_tst_lengths = torch.LongTensor([phones.size(0)]).to(device)
         del phones
@@ -385,8 +347,6 @@ def infer_multilang(
                 tones,
                 lang_ids,
                 bert,
-                ja_bert,
-                en_bert,
                 sdp_ratio=sdp_ratio,
                 noise_scale=noise_scale,
                 noise_scale_w=noise_scale_w,
@@ -403,8 +363,6 @@ def infer_multilang(
             bert,
             x_tst_lengths,
             speakers,
-            ja_bert,
-            en_bert,
         )  # , emo
         if torch.cuda.is_available():
             torch.cuda.empty_cache()
