39d4616814ddcd45625d60c90833b4094e96ed47
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-03 14:32:44 +0800

    Update infer.py

--- infer.py
+++ infer.py
@@ -11,7 +11,7 @@ import torch
 import commons
 from text import cleaned_text_to_sequence, get_bert
 
-# from clap_wrapper import get_clap_audio_feature, get_clap_text_feature
+from clap_wrapper import get_clap_audio_feature, get_clap_text_feature
 from text.cleaner import clean_text
 import utils
 import numpy as np
