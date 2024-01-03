63864ac9b39f60057e1f2906438529b89ce29f82
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-03 22:11:43 +0800

    Update clap_wrapper.py

--- clap_wrapper.py
+++ clap_wrapper.py
@@ -14,12 +14,12 @@ def get_clap_audio_feature(audio_data, device=config.bert_gen_config.device):
         inputs = processor(
             audios=audio_data, return_tensors="pt", sampling_rate=48000
         ).to(device)
-        emb = models[device].get_audio_features(**inputs)
+        emb = models.get_audio_features(**inputs)
     return emb.T
 
 
 def get_clap_text_feature(text, device=config.bert_gen_config.device):
     with torch.no_grad():
         inputs = processor(text=text, return_tensors="pt").to(device)
-        emb = models[device].get_text_features(**inputs)
+        emb = models.get_text_features(**inputs)
     return emb.T
