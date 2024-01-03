23d732748a7db8ea9ced1a9a83ebb667d36f2035
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-03 12:09:50 +0800

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -98,7 +98,7 @@ class TextAudioSpeakerLoader(torch.utils.data.Dataset):
         sid = torch.LongTensor([int(self.spk_map[sid])])
         if np.random.rand() > 0.1:
             emo = torch.squeeze(
-                torch.load(audiopath.replace(".wav", ".emo.npy"), map_location="cpu"),
+                torch.load(audiopath.replace(".wav", ".emo.pt"), map_location="cpu"),
                 dim=1,
             )
         else:
