c6ce255445f1b7c5e96f16c95546c51ff17740d4
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-07 00:13:16 +0800

    Update gen_phones.py

--- tools/gen_phones.py
+++ tools/gen_phones.py
@@ -5,12 +5,12 @@ import commons
 from text import cleaned_text_to_sequence
 from text.cleaner import clean_text
 
-def gen_phones(text, language_str, hps, style_text=None, style_weight=0.7):
+def gen_phones(text, language_str, add_blank, style_text=None, style_weight=0.7):
     style_text = None if style_text == "" else style_text
     # 在此处实现当前版本的get_text
     norm_text, phone, tone, word2ph = clean_text(text, language_str)
     phone, tone, language = cleaned_text_to_sequence(phone, tone, language_str)
-    if hps.data.add_blank:
+    if add_blank:
         phone = commons.intersperse(phone, 0)
         tone = commons.intersperse(tone, 0)
         language = commons.intersperse(language, 0)
