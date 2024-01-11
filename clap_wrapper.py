5199a7d69bf4fefc3791c735d0032d1099c4718f
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-11 10:11:51 +0800

    Update clap_wrapper.py

--- clap_wrapper.py
+++ clap_wrapper.py
@@ -6,7 +6,8 @@ from transformers import ClapModel, ClapProcessor
 from config import config
 
 models = dict()
-processor = ClapProcessor.from_pretrained("./emotional/clap-htsat-fused")
+LOCAL_PATH = "./emotional/clap-htsat-fused"
+processor = ClapProcessor.from_pretrained(LOCAL_PATH)
 
 
 def get_clap_audio_feature(audio_data, device=config.bert_gen_config.device):
@@ -18,10 +19,14 @@ def get_clap_audio_feature(audio_data, device=config.bert_gen_config.device):
         device = "mps"
     if not device:
         device = "cuda"
+    assert not (config.webui_config['fp16_run'] and config.webui_config['int8_run']), "fp16_run and int8_run cannot be both True"
     if device not in models.keys():
-        models[device] = ClapModel.from_pretrained("./emotional/clap-htsat-fused").to(
-            device
-        )
+        if config.webui_config['fp16_run']:
+            model = ClapModel.from_pretrained(LOCAL_PATH, torch_dtype=torch.float16).to(device)
+        elif config.webui_config['int8_run']:
+            model = ClapModel.from_pretrained(LOCAL_PATH, torch_dtype=torch.int8).to(device)
+        else:
+            model = ClapModel.from_pretrained(LOCAL_PATH).to(device)
     with torch.no_grad():
         inputs = processor(
             audios=audio_data, return_tensors="pt", sampling_rate=48000
@@ -39,10 +44,14 @@ def get_clap_text_feature(text, device=config.bert_gen_config.device):
         device = "mps"
     if not device:
         device = "cuda"
+    assert not (config.webui_config['fp16_run'] and config.webui_config['int8_run']), "fp16_run and int8_run cannot be both True"
     if device not in models.keys():
-        models[device] = ClapModel.from_pretrained("./emotional/clap-htsat-fused").to(
-            device
-        )
+        if config.webui_config['fp16_run']:
+            model = ClapModel.from_pretrained(LOCAL_PATH, torch_dtype=torch.float16).to(device)
+        elif config.webui_config['int8_run']:
+            model = ClapModel.from_pretrained(LOCAL_PATH, torch_dtype=torch.int8).to(device)
+        else:
+            model = ClapModel.from_pretrained(LOCAL_PATH).to(device)
     with torch.no_grad():
         inputs = processor(text=text, return_tensors="pt").to(device)
         emb = models[device].get_text_features(**inputs)
