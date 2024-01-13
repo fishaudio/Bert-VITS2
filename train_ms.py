1c470dd8d6edeb32264ec1b687613f50359e0d00
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-14 00:15:58 +0800

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -591,7 +591,7 @@ def train_and_evaluate(
                 loss_gen_all = loss_gen + loss_fm + loss_mel + loss_dur + loss_kl
                 if net_dur_disc is not None:
                     loss_dur_gen, losses_dur_gen = generator_loss(y_dur_hat_g)
-                    loss_gen_all += loss_dur_gen + loss_lm + loss_lm_gen
+                    loss_gen_all += loss_dur_gen + loss_lm + loss_lm_gen if net_wd is not None else loss_dur_gen
         optim_g.zero_grad()
         scaler.scale(loss_gen_all).backward()
         scaler.unscale_(optim_g)
