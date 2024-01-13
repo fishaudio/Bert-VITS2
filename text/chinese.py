ef6cf782799eddfade93498b85f1c40ad696579f
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-13 14:11:06 +0800

    Update chinese.py

--- text/chinese.py
+++ text/chinese.py
@@ -3,7 +3,7 @@ import re
 
 import cn2an
 from pypinyin import Style
-from pypinyin_g2pw import G2PWPinyin
+from pypinyin-G2pW-bv2 import G2PWPinyin
 from text.symbols import punctuation
 from text.tone_sandhi import ToneSandhi
 
