4eda202d5f0491eeb645d4d0463cdd028ae155e9
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-13 22:18:59 +0800

    Update models.py

--- models.py
+++ models.py
@@ -400,7 +400,7 @@ class TextEncoder(nn.Module):
         )
         self.proj = nn.Conv1d(hidden_channels, out_channels * 2, 1)
 
-    def forward(self, x, x_lengths, tone, language, bert, emo, g=None):
+    def forward(self, x, x_lengths, tone, language, bert, en_bert, emo, g=None):
         bert_emb = self.bert_proj(self.bert_pre_proj(bert)).transpose(1, 2)
         en_bert_emb = self.en_bert_proj(en_bert).transpose(1, 2)
         emo_emb = self.in_feature_net(emo)
