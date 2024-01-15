078654203f99fe1e754202370f3e9609d80d0ff2
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-15 11:49:10 +0800

    Update english_bert_mock.py

--- text/english_bert_mock.py
+++ text/english_bert_mock.py
@@ -28,12 +28,9 @@ def get_bert_feature(
         device = "mps"
     if not device:
         device = "cuda"
-    assert not (config.webui_config.fp16_run and config.webui_config.int8_run), "fp16_run and int8_run cannot be both True"
     if device not in models.keys():
         if config.webui_config.fp16_run:
             models[device] = DebertaV2Model.from_pretrained(LOCAL_PATH, torch_dtype=torch.float16).to(device)
-        elif config.webui_config.int8_run:
-            models[device] = DebertaV2Model.from_pretrained(LOCAL_PATH, torch_dtype=torch.int8).to(device)
         else:
             models[device] = DebertaV2Model.from_pretrained(LOCAL_PATH).to(device)
     with torch.no_grad():
