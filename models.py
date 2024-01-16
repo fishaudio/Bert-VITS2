ec3a93377363de7d5eaaa3f9e0a358a85af0c4cc
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-16 19:40:30 +0800

    Update models.py

--- models.py
+++ models.py
@@ -363,7 +363,7 @@ class TextEncoder(nn.Module):
         nn.init.normal_(self.language_emb.weight, 0.0, hidden_channels**-0.5)
         self.bert_proj = nn.Conv1d(1024, hidden_channels, 1)
         self.bert_pre_proj = nn.Conv1d(2048, 1024, 1)
-        self.en_bert_proj = nn.Conv1d(1024, hidden_channels, 1)
+        #self.en_bert_proj = nn.Conv1d(1024, hidden_channels, 1)
         self.in_feature_net = nn.Sequential(
             # input is assumed to an already normalized embedding
             nn.Linear(512, 1028, bias=False),
@@ -400,9 +400,9 @@ class TextEncoder(nn.Module):
         )
         self.proj = nn.Conv1d(hidden_channels, out_channels * 2, 1)
 
-    def forward(self, x, x_lengths, tone, language, bert, en_bert, emo, g=None):
+    def forward(self, x, x_lengths, tone, language, bert, emo, g=None):
         bert_emb = self.bert_proj(self.bert_pre_proj(bert)).transpose(1, 2)
-        en_bert_emb = self.en_bert_proj(en_bert).transpose(1, 2)
+        #en_bert_emb = self.en_bert_proj(en_bert).transpose(1, 2)
         emo_emb = self.in_feature_net(emo)
         emo_emb, _, loss_commit = self.emo_vq(emo_emb.unsqueeze(1))
         loss_commit = loss_commit.mean()
@@ -412,7 +412,7 @@ class TextEncoder(nn.Module):
             + self.tone_emb(tone)
             + self.language_emb(language)
             + bert_emb
-            + en_bert_emb
+            #+ en_bert_emb
             + emo_emb
         ) * math.sqrt(
             self.hidden_channels
@@ -973,7 +973,6 @@ class SynthesizerTrn(nn.Module):
         tone,
         language,
         bert,
-        en_bert,
         emo,
     ):
         if self.n_speakers > 0:
@@ -981,7 +980,7 @@ class SynthesizerTrn(nn.Module):
         else:
             g = self.ref_enc(y.transpose(1, 2)).unsqueeze(-1)
         x, m_p, logs_p, x_mask, loss_commit = self.enc_p(
-            x, x_lengths, tone, language, bert, en_bert, emo, g=g
+            x, x_lengths, tone, language, bert, emo, g=g
         )
         z, m_q, logs_q, y_mask = self.enc_q(y, y_lengths, g=g)
         z_p = self.flow(z, y_mask, g=g)
@@ -1061,7 +1060,6 @@ class SynthesizerTrn(nn.Module):
         tone,
         language,
         bert,
-        en_bert,
         emo,
         noise_scale=0.667,
         length_scale=1,
@@ -1077,7 +1075,7 @@ class SynthesizerTrn(nn.Module):
         else:
             g = self.ref_enc(y.transpose(1, 2)).unsqueeze(-1)
         x, m_p, logs_p, x_mask, _ = self.enc_p(
-            x, x_lengths, tone, language, bert, en_bert, emo, g=g
+            x, x_lengths, tone, language, bert, emo, g=g
         )
         logw = self.sdp(x, x_mask, g=g, reverse=True, noise_scale=noise_scale_w) * (
             sdp_ratio
