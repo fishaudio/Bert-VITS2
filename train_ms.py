72233628a6e4609e348b1b0e535a296fc0c109d9
Stardust·减 <star_dust_chen@foxmail.com>
2024-01-30 11:19:16 +0800

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -585,12 +585,13 @@ def train_and_evaluate(
 
                 loss_fm = feature_loss(fmap_r, fmap_g)
                 loss_gen, losses_gen = generator_loss(y_d_hat_g)
+                loss_commit = loss_commit * hps.train.c_commit
 
-                loss_gen_all = loss_gen + loss_fm + loss_mel + loss_dur + loss_kl
+                loss_gen_all = loss_gen + loss_fm + loss_mel + loss_dur + loss_kl + loss_commit
                 if net_dur_disc is not None:
                     loss_dur_gen, losses_dur_gen = generator_loss(y_dur_hat_g)
                     loss_gen_all += (
-                        loss_dur_gen + loss_lm + loss_lm_gen
+                        loss_dur_gen + loss_lm + loss_lm_gen + + loss_commit
                         if net_wd is not None
                         else loss_dur_gen
                     )
