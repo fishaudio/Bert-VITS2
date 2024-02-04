48e24c020b0fa152960ebcd7283b7e5a4e533b67
litagin02 <litagin02@gmail.com>
2024-02-04 18:33:09 +0900

    Add skip jvnv model download (useful for colab etc)

--- initialize.py
+++ initialize.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 from pathlib import Path
 
@@ -89,6 +90,10 @@ def download_jvnv_models():
             )
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--skip_jvnv", action="store_true")
+args = parser.parse_args()
+
 download_bert_models()
 
 download_slm_model()
@@ -97,4 +102,5 @@ download_pretrained_models()
 
 download_jp_extra_pretrained_models()
 
-download_jvnv_models()
+if not args.skip_jvnv:
+    download_jvnv_models()
