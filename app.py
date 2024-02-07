8b27f1b65cf254d6419d2392984f18770a1e5d21
litagin02 <litagin02@gmail.com>
2024-02-07 12:06:37 +0900

    Fix info of jvnv model

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ examples = [
 initial_md = """
 # Style-Bert-VITS2 ver 2.0 音声合成
 
-注意: 初期からある[jvnvのモデル](https://huggingface.co/litagin/style_bert_vits2_jvnv)は、[JVNVコーパス（言語音声と非言語音声を持つ日本語感情音声コーパス）](https://sites.google.com/site/shinnosuketakamichi/research-topics/jvnv_corpus)で学習されたモデルです、今のところは**JP-Extra版ではありません**。ライセンスは[CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/deed.ja)です。
+注意: 初期からある[jvnvのモデル](https://huggingface.co/litagin/style_bert_vits2_jvnv)は、[JVNVコーパス（言語音声と非言語音声を持つ日本語感情音声コーパス）](https://sites.google.com/site/shinnosuketakamichi/research-topics/jvnv_corpus)で学習されたモデルです。ライセンスは[CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/deed.ja)です。
 """
 
 how_to_md = """
