dd867c0724e9300803b27f652ffed4ec8f910a0f
litagin02 <litagin02@gmail.com>
2024-02-26 18:25:39 +0900

    Fix typo

--- Editor.bat
+++ Editor.bat
@@ -2,7 +2,7 @@ chcp 65001 > NUL
 @echo off
 
 pushd %~dp0
-echo Running server_editor.py --inbroser
+echo Running server_editor.py --inbrowser
 venv\Scripts\python server_editor.py --inbrowser
 
 if %errorlevel% neq 0 ( pause & popd & exit /b %errorlevel% )
