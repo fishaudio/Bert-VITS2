a20e8e377d6aa737a1e7b1c3222fd67d7af397f7
Ναρουσέ·μ·γιουμεμί·Χινακάννα <40709280+NaruseMioShirakana@users.noreply.github.com>
2024-02-27 23:12:02 +0800

    Add 2.4 Onnx Export

--- export_onnx.py
+++ export_onnx.py
@@ -7,8 +7,9 @@ if __name__ == "__main__":
     config_path = "model\\config.json"
     novq = False
     dev = False
+    Extra = "chinese"  #japanese or chinese
     if not os.path.exists("onnx"):
         os.makedirs("onnx")
     if not os.path.exists(f"onnx/{export_path}"):
         os.makedirs(f"onnx/{export_path}")
-    export_onnx(export_path, model_path, config_path, novq, dev)
+    export_onnx(export_path, model_path, config_path, novq, dev, Extra)
