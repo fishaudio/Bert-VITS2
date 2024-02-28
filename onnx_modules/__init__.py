2644f374a3a037767cc5d6a13174548e722ac50d
Ναρουσέ·μ·γιουμεμί·Χινακάννα <40709280+NaruseMioShirakana@users.noreply.github.com>
2024-02-28 13:08:59 +0800

    Config Fix

--- onnx_modules/__init__.py
+++ onnx_modules/__init__.py
@@ -6,6 +6,11 @@ def export_onnx(export_path, model_path, config_path, novq, dev, Extra):
     hps = get_hparams_from_file(config_path)
     version = hps.version[0:3]
     enable_emo = False
+    BertPaths = [
+            "chinese-roberta-wwm-ext-large",
+            "deberta-v2-large-japanese",
+            "bert-base-japanese-v3",
+        ]
     if version == "2.0" or (version == "2.1" and novq):
         from .V200 import SynthesizerTrn, symbols
     elif version == "2.1" and (not novq):
@@ -18,6 +23,7 @@ def export_onnx(export_path, model_path, config_path, novq, dev, Extra):
             from .V220 import SynthesizerTrn, symbols
     elif version == "2.3":
         from .V230 import SynthesizerTrn, symbols
+        BertPaths[1] = "deberta-v2-large-japanese-char-wwm"
     elif version == "2.4":
         enable_emo = True
         if Extra == "chinese":
@@ -41,13 +47,11 @@ def export_onnx(export_path, model_path, config_path, novq, dev, Extra):
         spklist.append(key)
 
     LangDict = {"ZH": [0, 0], "JP": [1, 6], "EN": [2, 8]}
-    BertPaths = [
-            "chinese-roberta-wwm-ext-large",
-            "deberta-v2-large-japanese",
-            "bert-base-japanese-v3",
-        ]
+    BertSize = 1024
     if version == "2.4":
-        BertPaths = (["chinese-roberta-wwm-ext-large"] if Extra == "chinese" else ["deberta-v2-large-japanese"])
+        BertPaths = (["Erlangshen-MegatronBert-1.3B-Chinese"] if Extra == "chinese" else ["deberta-v2-large-japanese-char-wwm"])
+        if Extra == "chinese":
+            BertSize = 2048
 
     MoeVSConf = {
         "Folder": f"{export_path}",
@@ -62,6 +66,7 @@ def export_onnx(export_path, model_path, config_path, novq, dev, Extra):
         "Dict": "BasicDict",
         "BertPath": BertPaths,
         "Clap": ("clap-htsat-fused" if enable_emo else False),
+        "BertSize": BertSize
     }
 
     with open(f"onnx/{export_path}.json", "w") as MoeVsConfFile:
