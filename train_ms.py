7d0aa66f9df7e4cec4581a4d6eccbbe6c27f5971
Yuzhou Tan <59681912+tanyuzhou@users.noreply.github.com>
2024-06-25 18:48:06 -0700

    fix args parser for train_ms (#378)
    fix the bug for https://github.com/fishaudio/Bert-VITS2/commit/81e034b1b7e0937262eae82e6f30b640e2776bb3

--- train_ms.py
+++ train_ms.py
@@ -99,7 +99,7 @@ def run():
         help="数据集文件夹路径，请注意，数据不再默认放在/logs文件夹下。如果需要用命令行配置，请声明相对于根目录的路径",
         default=config.dataset_path,
     )
-    args = parser.parse_known_args()
+    args, _ = parser.parse_known_args()
     model_dir = os.path.join(args.model, config.train_ms_config.model)
     if not os.path.exists(model_dir):
         os.makedirs(model_dir, exist_ok=True)
