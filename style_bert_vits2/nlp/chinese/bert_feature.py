7793e9da432444308737fa17d2ddb0d5859bed03
tsukumi <tsukumijima@users.noreply.github.com>
2024-09-28 13:50:10 +0900

    Remove: Unused import

--- style_bert_vits2/nlp/chinese/bert_feature.py
+++ style_bert_vits2/nlp/chinese/bert_feature.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Optional, Sequence, Union, cast
+from typing import TYPE_CHECKING, Any, Optional, Sequence, Union
 
 import numpy as np
 from numpy.typing import NDArray
