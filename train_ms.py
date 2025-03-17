52c340d6919b6cb3627d2fad4c107e8499bdd97b
Stardust·减 <star_dust_chen@foxmail.com>
2025-03-17 15:44:37 +0800

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -583,7 +583,7 @@ def train_and_evaluate(
                 loss_dur = torch.sum(l_length.float())
                 loss_mel = F.l1_loss(y_mel, y_hat_mel) * hps.train.c_mel
                 # loss_kl = kl_loss(z_p, logs_q, m_p, logs_p, z_mask) * hps.train.c_kl  # Old implement
-                loss_kl_text = kl_loss_normal(m_q_text, logs_q_text, m_p_text, logs_p_text, x_mask) * 0.1
+                loss_kl_text = kl_loss_normal(m_q_text, logs_q_text, m_p_text, logs_p_text, x_mask) * 0.01
                 loss_kl_dur = kl_loss(z_q_dur, logs_q_dur, m_p_dur, logs_p_dur, z_mask) * 2
                 loss_kl_audio = kl_loss_normal(m_p_audio, logs_p_audio, m_q_audio, logs_q_audio, z_mask) * 0.05
 
@@ -595,9 +595,9 @@ def train_and_evaluate(
                     + loss_fm
                     + loss_mel
                     + loss_dur
-                    + loss_kl_text
+                    #+ loss_kl_text
                     + loss_kl_dur 
-                    + loss_kl_audio
+                    #+ loss_kl_audio
                     + loss_commit * hps.train.c_commit
                 )
                 if net_dur_disc is not None:
